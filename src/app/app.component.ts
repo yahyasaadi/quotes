87e6fd9979126c0af54da91d15a5b294d0dc046f
yahyasaadi <yahyasaadi9219@gmail.com>
2021-12-22 12:40:56 +0300

    add a function to add a new quote from the user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,4 @@ import { Quotes } from './quotes';
 export class AppComponent {
   title = 'Quotes';
 
-  quotes:Quotes[] = [
-    new Quotes('Veni, vidi, vici', 'Julius Caesar', 'Yahya Saadi'),
-    new Quotes('I was born at a very young age', 'Benjamin Button', 'Yahya Saadi'),
-    new Quotes('We are not here to take part, we are here to take over', 'Conor McGregor', 'Yahya Saadi')
-  ];
 }
--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+  public quote = '';
+  public quoteAuthor = '';
+  public name = '';
+
+  // quotes:Quotes[] = [
+  //   new Quotes('Veni, vidi, vici', 'Julius Caesar', 'Yahya Saadi'),
+  //   new Quotes('I was born at a very young age', 'Benjamin Button', 'Yahya Saadi'),
+  //   new Quotes('We are not here to take part, we are here to take over', 'Conor McGregor', 'Yahya Saadi')
+  // ];
+
+  quotes: Quotes[] = [
+    {
+      quote: 'Veni, vidi, vici',
+      quoteAuthor: 'Julius Caesar',
+      name: 'Yahya Saadi'
+    },
+    {
+      quote: 'I was born at a very young age',
+      quoteAuthor: 'Benjamin Button',
+      name: 'Yahya Saadi'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addQuote(){
+    let newQuote = {
+      quote : this.quote,
+      quoteAuthor : this.quoteAuthor,
+      name : this.name
+    }
+    this.quotes.push(newQuote)
   }
 
 }
