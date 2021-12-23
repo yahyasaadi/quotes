77bba573df49be6599732cf2896e873ff52ae919
yahyasaadi <yahyasaadi9219@gmail.com>
2021-12-23 10:20:31 +0300

    added a navbar with styling

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
+// This too does not work. Might delete
+import { QuoteFormComponent } from '../quote-form/quote-form.component';  
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
