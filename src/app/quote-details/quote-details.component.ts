87374a51e82e9853ead63da775b47a8c206a9dc2
yahyasaadi <yahyasaadi9219@gmail.com>
2022-01-03 13:24:46 +0300

    add an event emitter

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quotes } from '../quotes';
 // This too does not work. Might delete
 import { QuoteFormComponent } from '../quote-form/quote-form.component';  
@@ -10,9 +10,15 @@ import { QuoteFormComponent } from '../quote-form/quote-form.component';
 export class QuoteDetailsComponent implements OnInit {
 
   @Input() childQuote!: Quotes
+  @Output() isClicked = new EventEmitter<boolean>()
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // Function to delete a function
+  deleteQuote(qToDelete: Quotes){
+    
   }
 
 }
